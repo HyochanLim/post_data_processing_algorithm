7005e004da8dbf036afbae9fb154960e7d8c7149
Ivan Lim <hyochan5211@snu.ac.kr>
2026-03-04 22:57:31 +0900

    bug fixed

---
+++ logic/status.js
@@ -0,0 +1,13 @@
+const three = require('three');
+
+let time, vx, vy, vz, ax, ay, az;
+
+module.exports = {
+    time: time,
+    vx: vx,
+    vy: vy,
+    vz: vz,
+    ax: ax,
+    ay: ay,
+    az: az
+}
